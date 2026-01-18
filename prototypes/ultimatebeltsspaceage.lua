559ea40180a05f54e52549ffe54f2ae7a4ba8858
kryojenik <kryojenik2@gmail.com>
2026-01-18 02:23:33 -0600

    Fix recipe ordering

--- prototypes/ultimatebeltsspaceage.lua
+++ prototypes/ultimatebeltsspaceage.lua
@@ -7,6 +7,7 @@ templates.loaders = {
     next_upgrade = "extreme-fast-mdrn-loader",
     previous_prefix = "express-",
     tint = util.color("00ac08d1"),
+    order = "05",
     prerequisite_techs = {
       "ultra-fast-logistics",
       "express-mdrn-loader"
@@ -27,6 +28,7 @@ templates.loaders = {
   ["extreme-fast-"] = {
     next_upgrade = "ultra-express-mdrn-loader",
     tint = util.color("db071fd1"),
+    order = "06",
     prerequisite_techs = { "extreme-fast-logistics", "ultra-fast-mdrn-loader" },
     recipe_data = {
       ingredients = {
@@ -44,6 +46,7 @@ templates.loaders = {
   ["ultra-express-"] = {
     next_upgrade = "extreme-express-mdrn-loader",
     tint = util.color("4a01d8d1"),
+    order = "07",
     prerequisite_techs = {
       "ultra-express-logistics",
       "extreme-fast-mdrn-loader"
@@ -64,6 +67,7 @@ templates.loaders = {
   ["extreme-express-"] = {
     next_upgrade = "original-ultimate-mdrn-loader",
     tint = util.color("0620d6d1"),
+    order = "08",
     prerequisite_techs = {
       "extreme-express-logistics",
       "ultra-express-mdrn-loader"
@@ -83,6 +87,7 @@ templates.loaders = {
   },
   ["original-ultimate-"] = {
     tint = util.color("06d9c4d1"),
+    order = "09",
     prerequisite_techs = {
       "ultimate-logistics",
       "extreme-express-mdrn-loader"
