ee73954daafa392800d0780fc6b785ecc436bfc0
kryojenik <kryojenik2@gmail.com>
2026-04-24 00:42:11 -0500

    Locale updates and version bump to 2.0.0

--- data-updates.lua
+++ data-updates.lua
@@ -1,6 +1,8 @@
 -- Ultimate Belts Space Age adjusts logistics-3 tech cost in data-updates.
 -- Adjust to unlock at same science tier
+local C   = require("__loaders-modernized__.constants")
+local cfg = require("__loaders-modernized__.prototypes.settings-cache")
 
-if settings.startup["mdrn-unlock-technology"].value == "separate" then
-  data.raw["technology"]["express-mdrn-loader"].unit = data.raw["technology"]["logistics-3"].unit
+if cfg.unlock_technology == C.UNLOCK_TECH.SEPARATE then
+  data.raw["technology"]["mdrn-express-loader"].unit = data.raw["technology"]["logistics-3"].unit
 end
--- prototypes/ultimatebeltsspaceage.lua
+++ prototypes/ultimatebeltsspaceage.lua
@@ -3,84 +3,84 @@
 local loaders = {
   ["ultra-fast-"] = {
     --Ultimate belts move on passed express and ignore Space Age! Turbo belts
-    next_upgrade = "extreme-fast-mdrn-loader",
-    upgrade_from_prefix = "express-",
+    next_upgrade = "mdrn-extreme-fast-loader",
+    upgrade_from_tier = "express-",
     tint = util.color("00ac08d1"),
     order = "05",
     prerequisite_techs = {
       "ultra-fast-logistics",
-      "express-mdrn-loader"
+      "mdrn-express-loader"
     },
     recipe_data = {
       ingredients = {
         { type = "item", name = "ultra-fast-underground-belt", amount = 1 },
         { type = "item", name = "bulk-inserter", amount = 3 },
-        { type = "item", name = "express-mdrn-loader", amount = 1 },
+        { type = "item", name = "mdrn-express-loader", amount = 1 },
       },
       stack_ingredients = {
         { type = "item", name = "ultra-fast-underground-belt", amount = 1 },
         { type = "item", name = "bulk-inserter", amount = 6 },
-        { type = "item", name = "express-mdrn-loader", amount = 1 },
+        { type = "item", name = "mdrn-express-loader", amount = 1 },
       }
     }
   },
   ["extreme-fast-"] = {
-    next_upgrade = "ultra-express-mdrn-loader",
+    next_upgrade = "mdrn-ultra-express-loader",
     tint = util.color("db071fd1"),
     order = "06",
-    prerequisite_techs = { "extreme-fast-logistics", "ultra-fast-mdrn-loader" },
+    prerequisite_techs = { "extreme-fast-logistics", "mdrn-ultra-fast-loader" },
     recipe_data = {
       ingredients = {
         { type = "item", name = "extreme-fast-underground-belt", amount = 1 },
         { type = "item", name = "bulk-inserter", amount = 3 },
-        { type = "item", name = "ultra-fast-mdrn-loader", amount = 1 },
+        { type = "item", name = "mdrn-ultra-fast-loader", amount = 1 },
       },
       stack_ingredients = {
         { type = "item", name = "extreme-fast-underground-belt", amount = 1 },
         { type = "item", name = "bulk-inserter", amount = 6 },
-        { type = "item", name = "ultra-fast-mdrn-loader", amount = 1 },
+        { type = "item", name = "mdrn-ultra-fast-loader", amount = 1 },
       }
     }
   },
   ["ultra-express-"] = {
-    next_upgrade = "extreme-express-mdrn-loader",
+    next_upgrade = "mdrn-extreme-express-loader",
     tint = util.color("4a01d8d1"),
     order = "07",
     prerequisite_techs = {
       "ultra-express-logistics",
-      "extreme-fast-mdrn-loader"
+      "mdrn-extreme-fast-loader"
     },
     recipe_data = {
       ingredients = {
         { type = "item", name = "ultra-express-underground-belt", amount = 1 },
         { type = "item", name = "bulk-inserter", amount = 3 },
-        { type = "item", name = "extreme-fast-mdrn-loader", amount = 1 },
+        { type = "item", name = "mdrn-extreme-fast-loader", amount = 1 },
       },
       stack_ingredients = {
         { type = "item", name = "ultra-express-underground-belt", amount = 1 },
         { type = "item", name = "bulk-inserter", amount = 6 },
-        { type = "item", name = "extreme-fast-mdrn-loader", amount = 1 },
+        { type = "item", name = "mdrn-extreme-fast-loader", amount = 1 },
       }
     }
   },
   ["extreme-express-"] = {
-    next_upgrade = "original-ultimate-mdrn-loader",
+    next_upgrade = "mdrn-original-ultimate-loader",
     tint = util.color("0620d6d1"),
     order = "08",
     prerequisite_techs = {
       "extreme-express-logistics",
-      "ultra-express-mdrn-loader"
+      "mdrn-ultra-express-loader"
     },
     recipe_data = {
       ingredients = {
         { type = "item", name = "extreme-express-underground-belt", amount = 1 },
         { type = "item", name = "bulk-inserter", amount = 3 },
-        { type = "item", name = "ultra-express-mdrn-loader", amount = 1 },
+        { type = "item", name = "mdrn-ultra-express-loader", amount = 1 },
       },
       stack_ingredients = {
         { type = "item", name = "extreme-express-underground-belt", amount = 1 },
         { type = "item", name = "bulk-inserter", amount = 6 },
-        { type = "item", name = "ultra-express-mdrn-loader", amount = 1 },
+        { type = "item", name = "mdrn-ultra-express-loader", amount = 1 },
       }
     }
   },
@@ -89,18 +89,18 @@ local loaders = {
     order = "09",
     prerequisite_techs = {
       "ultimate-logistics",
-      "extreme-express-mdrn-loader"
+      "mdrn-extreme-express-loader"
     },
     recipe_data = {
       ingredients = {
         { type = "item", name = "original-ultimate-underground-belt", amount = 1 },
         { type = "item", name = "bulk-inserter", amount = 3 },
-        { type = "item", name = "extreme-express-mdrn-loader", amount = 1 },
+        { type = "item", name = "mdrn-extreme-express-loader", amount = 1 },
       },
       stack_ingredients = {
         { type = "item", name = "original-ultimate-underground-belt", amount = 1 },
         { type = "item", name = "bulk-inserter", amount = 6 },
-        { type = "item", name = "extreme-express-mdrn-loader", amount = 1 },
+        { type = "item", name = "mdrn-extreme-express-loader", amount = 1 },
       }
     }
   },
